cf0d2c26aa0ba094d5987bf7edbf25f6b050a8b0
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2025-05-23 16:04:06 -0300

    chore: setup projetct with cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  experimentalModuleVariables: true,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "https://erickwendel.github.io/vanilla-js-web-app-example",
+    testIsolation: false,
+  },
+});
